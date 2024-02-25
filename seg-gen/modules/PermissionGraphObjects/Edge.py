869c0df2344a65efa36158243aab12d580e2262d
Randy Truong <rtruong@u.northwestern.edu>
2024-02-24 19:05:30 -0600

    deprecated Edge

--- seg-gen/modules/PermissionGraphObjects/Edge.py
+++ seg-gen/modules/PermissionGraphObjects/Edge.py
@@ -1,10 +1,16 @@
-# Edge.py
+# Edge.py (MIGHT BE DEPRECATED LOL) 
 # 
 # Helper class for creating an `Edge` object 
 # 
 # Randy Truong 
 # Northwestern University 
 # 11 February 2024 
+
+from enum import Enum 
+
+class EdgeType(Enum): 
+    BELONGING_EDGE   = 1 
+    REQUEST_EDGE     = 2 
 
 class Edge: 
     def __init__(self, src: str, dst: str, edgeType: str):  
